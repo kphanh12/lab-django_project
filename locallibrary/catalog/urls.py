13965bf7d2f906bb01a1e35cbb418f3947314445
kphanh12 <kphanh1234@gmail.com>
2023-09-24 13:58:15 +0700

    OTHER-5 <lab_django>

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,23 @@
+# Use include() to add paths from the catalog application
+from django.urls import include
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+]
